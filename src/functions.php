057d82ab7d4c4990dfbe99b344564ca69c851ec6
Openset <openset.wang@gmail.com>
2018-06-28 11:00:27 +0800

    Add: is_cli

--- src/functions.php
+++ src/functions.php
@@ -1,1 +1,12 @@
 <?php
+
+if (!function_exists('is_cli')) {
+    /**
+     * Determine if the script is running in the console.
+     * @return bool
+     */
+    function is_cli()
+    {
+        return in_array(PHP_SAPI, ['cli', 'phpdbg']);
+    }
+}
