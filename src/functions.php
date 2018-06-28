a3984ceba66c4495d755fe4aba87e2e5152909ea
Openset <openset.wang@gmail.com>
2018-06-28 11:02:09 +0800

    Add: dump

--- src/functions.php
+++ src/functions.php
@@ -10,3 +10,27 @@ if (!function_exists('is_cli')) {
         return in_array(PHP_SAPI, ['cli', 'phpdbg']);
     }
 }
+
+if (!function_exists('dump')) {
+    /**
+     * Format print
+     * dump($arg, 1, '', 0, true);
+     * @param mixed ...$args
+     */
+    function dump(...$args)
+    {
+        $delimiters = is_cli() ? ['', PHP_EOL] : ['<pre>', '</pre>'];
+        $i = func_num_args() - 1;
+        foreach ($args as $k => $arg) {
+            if ($k == $i && is_bool($arg) && $arg) {
+                exit();
+            }
+            echo $delimiters[0];
+            if (empty($arg) || is_bool($arg)) {
+                $arg = json_encode($arg);
+            }
+            print_r($arg);
+            echo $delimiters[1];
+        }
+    }
+}
